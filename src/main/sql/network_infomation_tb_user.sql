7870bf300d417fb0ab0790c025cf2ca3959dde56
Yang Lin <963214153@qq.com>
2018-04-02 20:38:50 +0800

    更新SQL脚本，新增数据库选择

--- src/main/sql/network_infomation_tb_user.sql
+++ src/main/sql/network_infomation_tb_user.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `tb_user`
 --
-
+USE network_infomation;
 DROP TABLE IF EXISTS `tb_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
